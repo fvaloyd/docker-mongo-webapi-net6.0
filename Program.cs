eff75c7d8c0284133707e5cfbc4b4a829106ae43
Antsy15400 <francisalcantara001@gmail.com>
2022-05-12 08:44:07 -0400

    Unnecessary comments have been removed

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 using docker_mongo_webapi.databaseSetting;
 using docker_mongo_webapi.services;
-using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// JSON serialization
 builder.Services.AddControllers()
     .AddNewtonsoftJson(opt => opt.UseMemberCasing());
-
 builder.Services.AddControllers()
     .AddJsonOptions(
         options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
@@ -16,29 +15,21 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// DB Configuration
 builder.Services.Configure<MongoDbSettings>(
         builder.Configuration.GetSection("MongoDbSettings"));
 
+// Service
 builder.Services.AddSingleton<PeliculaServices>();
 
-// builder.Services.Configure<ForwardedHeadersOptions>(opt => 
-//         {
-//             opt.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-//         });
-//
 var app = builder.Build();
 
-// app.UseForwardedHeaders();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    // app.UseDeveloperExceptionPage();
 }
-// else {
-//     app.UseHsts();
-// }
 
 app.UseHttpsRedirection();
 
